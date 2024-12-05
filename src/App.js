f0af6887f1a2abd87982366f53052ae9895d4e1f
matsiashchik <m_vlad_f@hotmail.com>
2024-12-05 17:06:26 +0700

    login and registration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Login from "./components/login/Login";
+import Notification from "./norification/Notification";
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <></>
-  );
+    const user = false;
+
+    return (
+        <div>
+            {
+                user
+                    ? (<div>Chat page</div>)
+                    : (<Login/>)
+            }
+            <Notification/>
+        </div>
+    );
 }
 
 export default App;
